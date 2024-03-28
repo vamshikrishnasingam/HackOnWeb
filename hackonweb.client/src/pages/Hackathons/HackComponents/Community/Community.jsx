d3abf99869a8443ff8cbda50f23485dff6a092dc
Sai Charan Raachala <charanshashi029@gmail.com>
2024-03-28 13:18:35 +0530

    commit_28-03-24_13:15

--- hackonweb.client/src/pages/Hackathons/HackComponents/Community/Community.jsx
+++ hackonweb.client/src/pages/Hackathons/HackComponents/Community/Community.jsx
@@ -1,24 +1,47 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { PhotoIcon, UserCircleIcon} from '@heroicons/react/24/solid'
+import { IoCloudUploadOutline } from "react-icons/io5";
 
 function Community() {
     const hacname = "hackathon";
     const [image, setImage] = useState(null);
+    const [file, setFile] = useState(null);
 
     const handleImageChange = (e) => {
-        const file = e.target.files[0];
+        const img = e.target.files[0];
         const reader = new FileReader();
 
         reader.onloadend = () => {
             setImage(reader.result);
         };
 
-        if (file) {
-            reader.readAsDataURL(file);
+        if (img) {
+            reader.readAsDataURL(img);
         }
     };
+    const handleImageUplaod = () => {
+        //uplaod image to database
+        setImage(null);
+    }
+    const handleFileChange = (e) => {
+        const files = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setFile(reader.result);
+        };
+
+        if (files) {
+            reader.readAsDataURL(files);
+        }
+    };
+    const handleFileUplaod = () => {
+        //uplaod image to database
+        setFile(null);
+
+    }
 
 
     return (
@@ -42,16 +65,57 @@ function Community() {
                                         style={{ display: 'block', padding: '1rem', marginTop: '2rem' }}
                                     >
                                     <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                                         <div className="text-center">
-                                            <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                                            <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                                <span>Upload a file</span>
-                                                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                            <div className="text-center">
+                                                {image ? (
+                                                    <div className="mt-4 text-center">
+                                                        <img src={image} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '300px' }} />
+                                                        <button onClick={handleImageUplaod }>upload</button>
+                                                    </div>
+                                                ) : (
+                                                    <div>
+                                                         <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                                                         <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                                              <span>Upload a file</span>
+                                                              <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={handleImageChange} />
+                                                           </div>
+                                                           <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                                           <p className="pl-1">or drag and drop</p>
+                                                    </div>                                
+                                                ) }
                                             </div>
-                                            <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                                            <p className="pl-1">or drag and drop</p>
-                                         </div>
-                                     </div>
+                                    </div>
+                                    </label>
+                                </div>
+
+                                <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                                    File Upload
+                                </label>
+                                <div>
+                                    <label
+                                        htmlFor="file-upload"
+                                        className="block cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                                        style={{ display: 'block', padding: '1rem', marginTop: '2rem' }}
+                                    >
+                                        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                            <div className="text-center">
+                                                {file ? (
+                                                    <div className="mt-4 text-center">
+                                                        <img src={file} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '300px' }} />
+                                                        <button onClick={handleFileUplaod}>upload</button>
+                                                    </div>
+                                                ) : (
+                                                        <div>
+                                                            <IoCloudUploadOutline className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                                                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                                            <span>Upload a file</span>
+                                                            <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={handleFileChange} />
+                                                        </div>
+                                                        <p className="text-xs leading-5 text-gray-600">PDF,XML up to 10MB</p>
+                                                        <p className="pl-1">or drag and drop</p>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
                                     </label>
                                 </div>
 
