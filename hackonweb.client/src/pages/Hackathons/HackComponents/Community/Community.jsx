e40816ce9a041ce5d726ce2fcb32d6557694510b
shashidhar7682 <shashidhar7682@gmail.com>
2024-04-14 03:22:14 +0530

    commit_14/04/24_03:22

--- hackonweb.client/src/pages/Hackathons/HackComponents/Community/Community.jsx
+++ hackonweb.client/src/pages/Hackathons/HackComponents/Community/Community.jsx
@@ -26,14 +26,10 @@ function Community() {
     const [uploadedFiles, setUploadedFiles] = useState([]);
     const [uploadedImages, setUploadedImages] = useState([]);
     const [visibility, setVisibility] = useState(false);
-    useEffect(() => {
-        // Load images from local storage when component mounts
-        GetCommunityDetails();
-        console.log(communityDetails);
-    }, [showImages]);
 
     useEffect(() => {
-        GetCommunityDetails();
+        GetCommunityDetails
+        console.log(communityDetails);
     }, []);
 
     const GetCommunityDetails = async () => {
@@ -493,7 +489,8 @@ function Community() {
                                         id="about"
                                         name="about"
                                         rows={5}
-                                        className=" desc  block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="Describe your project"
+                                        className="desc  block w-full p-1 py-2 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         value={description} // Bind value to state variable
                                         onChange={handleDescriptionChange} // Call function on change
                                     />
