be85cc5617423511ed15348673bd25017a102d9d
Vamshi Krishna <vamshikrishnasingam1523@gmail.com>
2024-05-11 20:06:02 +0530

    commit_change_main.jsx

--- hackonweb.client/src/main.jsx
+++ hackonweb.client/src/main.jsx
@@ -5,10 +5,13 @@ import './index.css'
 import { Provider } from 'react-redux';
 import store from './pages/Hackathons/HackComponents/Hacks/Redux/store.jsx'
 import UserLoginStore from './contexts/UserLoginStore.jsx'
-import CommunityStore from './contexts/CommunityStore.jsx';
-
+import CommunityStore from './contexts/CommunityStore.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <UserLoginStore>
-        <App />
-    </UserLoginStore>
+    <Provider store={store}>
+        <UserLoginStore>
+            <CommunityStore>
+                <App />
+            </CommunityStore>
+        </UserLoginStore>
+    </Provider>
 )
